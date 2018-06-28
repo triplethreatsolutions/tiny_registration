9face6f89d19e0a64831d0900cce6799a09d70ce
Jason Bowman <jason.bowman@rockwellcollins.com>
2018-06-28 14:00:42 -0500

    added questions to scripts

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("Starting tinyTourneys Registration...")
+
+// Questions Array
+const questions = [
+  {question: 'Enter Email Address', pattern: /\S+@\S+\.\S+/},
+  {question: 'Enter Team Name'},
+  {question: 'Enter Head Coach Name'},
+  {question: 'Enter Director Name'},
+  {question: 'Create Password', type: 'password'},
+];
+
+// Transition Times
+const shakeTime = 100; // Shake transition time (ms)
+const switchTime = 200; //Transition between questions (ms)
+
+// Init Position at First Question
+let position = 0;
+
+// Init DOM elements
+
+
